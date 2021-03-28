f982400dd7c88d07683ff987a36ad10073c399a7
atul <atul92345@gmail.com>
2021-03-28 15:43:33 +0530

    setup

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@
      e.preventDefault()
       const location = search.value
     
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
      response.json().then((data)=>{
          if(data.error){
             message1.textContent=data.error
--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require('./forecast')
 const geocode = require('./geocode')
 
 const app = express()
-
+const port = process.env.PORT || 3000
 // Define paths for  Express Config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -89,6 +89,6 @@ app.get('*', (req,res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port '+ port)
 })
